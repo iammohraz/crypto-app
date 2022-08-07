d741222c49fbafe48afc24a3b83fe756a80b7d4c
mohammad_razavi <mohammad.razavi.ansari.5207@gmail.com>
2022-08-07 13:03:21 +0430

    Changing the name of axios and exchange view

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import axios from 'axios'
 
-const instance = axios.create({
+const axios_ins = axios.create({
     baseURL: 'https://api.coingecko.com/api/v3/'
 })
 
-export default {axios}
+export default {axios_ins}
 
 createApp(App).use(router).mount('#app')
