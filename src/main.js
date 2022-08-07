bed8f320f85580056efc5e337c906e7641c34cf3
mohammad_razavi <mohammad.razavi.ansari.5207@gmail.com>
2022-08-07 12:22:27 +0430

    Add Bootstrap and Axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'https://api.coingecko.com/api/v3/'
+})
+
+export default {axios}
 
 createApp(App).use(router).mount('#app')
